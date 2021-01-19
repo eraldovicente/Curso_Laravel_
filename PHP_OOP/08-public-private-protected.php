2499901ea88baebf02d7e1f6a5c6f37fcdf9b056
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-19 01:17:02 -0200

    Modificadores: public, private y protected

---
+++ PHP_OOP/08-public-private-protected.php
@@ -0,0 +1,61 @@
+<?php include 'includes/header.php';
+
+// Public - se puede acceder en cualquier lugar, (objeto o clase)
+// Protected - Se puede acceder unicamente en la clase
+// Private - No puede ser accedido por los hijos
+
+class MenuRestaurant {
+     protected $nombre; 
+     public $precio;
+
+     public function __construct( $nombre, $precio )
+     {
+          $this->nombre = $nombre;
+          $this->precio = $precio;
+     }
+
+     public function getNombre() 
+     {
+          return $this->nombre;
+     }
+
+     public function getPrecio()
+     {
+          return $this->precio;
+     }
+}
+
+class Bebida extends MenuRestaurant {
+     public $medida;
+
+     public function __construct( $nombre, $precio, $medida )
+     {
+          parent::__construct($nombre, $precio);
+          $this->medida = $medida;
+     }
+
+     public function getMedida()
+     {
+          return $this->medida;
+     }
+}
+
+class Postre extends MenuRestaurant {
+     public $peso;
+
+     public function __construct( $nombre, $precio, $peso )
+     {
+          parent::__construct($nombre, $precio);
+          $this->peso = $peso;
+     }
+
+     public function getPeso()
+     {
+          return $this->peso;
+     }
+}
+
+$postre = new Postre('Pastel de Chocolate', 100, '150g');
+
+echo $postre->nombre;
+// echo $postre->getNombre();
