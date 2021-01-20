3a34a4323e8aa6d526f5e402ce96664b9c7bba07
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-19 22:11:26 -0200

    Como pasar datos de controller a view

--- recetaslaravel/app/Http/Controllers/RecetaController.php
+++ recetaslaravel/app/Http/Controllers/RecetaController.php
@@ -14,6 +14,13 @@ class RecetaController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('recetas.index');
+        $recetas = ['Receta Pizza', 'Receta Hamburguesa', 'Receta Tacos'];
+        $categorias = ['Comida Mexicana', 'Comida Argentina', 'Postres'];
+
+        return view('recetas.index')
+                    ->with('recetas', $recetas)
+                    ->with('categorias', $categorias);
+
+        // return view('recetas.index', compact('recetas', 'categorias') );
     }
 }
